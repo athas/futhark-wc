2aa9cdeac164beb402a5b83b4946b5b9bf373d92
Troels Henriksen <athas@sigkill.dk>
2019-10-25 13:23:31 +0200

    Better output format.

--- wc.c
+++ wc.c
@@ -49,7 +49,8 @@ int main(int argc, char** argv) {
   size_t n = ftell(fp);
   rewind(fp);
 
-  void *data = mmap(NULL, n, PROT_READ, MAP_SHARED, fileno(fp), 0);
+  void *data =
+    mmap(NULL, n, PROT_READ, MAP_SHARED, fileno(fp), 0);
   assert(data != MAP_FAILED);
 
   struct futhark_u8_1d *arr =
@@ -59,7 +60,7 @@ int main(int argc, char** argv) {
   int chars, words, lines;
   futhark_entry_wc(ctx, &chars, &words, &lines, arr);
 
-  printf(" %d %d %d %s\n", lines, words, chars, filename);
+  printf("  %d %d %d %s\n", lines, words, chars, filename);
 
   futhark_free_u8_1d(ctx, arr);
   futhark_context_free(ctx);
@@ -68,7 +69,8 @@ int main(int argc, char** argv) {
   gettimeofday(&t_end, NULL);
 
   if (timeit) {
-    printf("runtime: %dus\n",
-           (int)((t_end.tv_sec*1000000+t_end.tv_usec) - (t_start.tv_sec*1000000+t_start.tv_usec)));
+    printf("runtime: %.3fs\n",
+           ((t_end.tv_sec*1000000+t_end.tv_usec) -
+            (t_start.tv_sec*1000000+t_start.tv_usec)) / 1000000.0);
   }
 }
